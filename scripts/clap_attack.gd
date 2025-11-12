f140051c2f43e856b10a2af2222a551fc9c493c0
Noah <104743939+buysnoah@users.noreply.github.com>
2025-11-12 20:18:12 +0200

    Update

--- scripts/clap_attack.gd
+++ scripts/clap_attack.gd
@@ -4,7 +4,13 @@ signal hit(body: PhysicsBody2D)
 
 @export var CLAP_SPEED: float = 1
 
+var is_attacking = false
+
 func attack():
+	if is_attacking:
+		return
+	
+	is_attacking = true
 	$Sprite2D.visible = true
 	
 	for body in $Area2D.get_overlapping_bodies():
@@ -12,6 +18,7 @@ func attack():
 	
 	await get_tree().create_timer(0.2).timeout
 	$Sprite2D.visible = false
+	is_attacking = false    
 
 func _ready() -> void:
 	$Timer.wait_time = 1 / CLAP_SPEED
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,15 @@
 extends Node
 
 @export var camera: Camera2D
+@export var enemy: PackedScene
+@export var player: Node2D
 
 func _ready() -> void:
 	camera.reparent($Player)
+	var e = enemy.instantiate()
+	e.TARGET = player
+	add_child(e)
+	
 
 func _process(_delta: float) -> void:
 	pass
